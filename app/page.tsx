317860b60fe221acaaa0837e74f7372b59251677
keperkjr <keperkjr@yahoo.com>
2024-02-11 21:07:24 -0800

    Auth protect home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row text-center">
                 <div className="col">
                     <h1>Home</h1>
                 </div>
